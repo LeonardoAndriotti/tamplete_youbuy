e6e7be264b27546de911597065dbdbb8fad7838e
ANYMARKET <dev@anymarket.com.br>
2017-11-22 12:49:50 -0200

    up

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+var express      = require('express');
+
+var app = express()
+  .use(express.static(__dirname + '/public'))
+  .listen(process.env.PORT || 5000);
